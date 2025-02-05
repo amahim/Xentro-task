49db9bb9b4b065f6d9ec04757d7d4a103c5c461f
amahim <arifbin71@gmail.com>
2025-02-05 22:42:22 +0600

    first commit

---
+++ src/Components/Homepage/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Navbar from './Navbar';
+
+const Home = () => {
+    return (
+        <div className='w-4/5 mx-auto py-5'>
+            <Navbar/>
+        </div>
+    );
+};
+
+export default Home;
